a77a8ce009539cc4c89a41db219adb855f4c33bc
김덕윤 <duk9741@gmail.com>
2026-02-19 13:54:02 +0900

    fix: GameResult 보너스 번호 매칭 로직 수정

--- src/main/java/com/kakao/onboarding/precourse/albusduke/lotto/domain/GameResult.java
+++ src/main/java/com/kakao/onboarding/precourse/albusduke/lotto/domain/GameResult.java
@@ -3,6 +3,9 @@ package com.kakao.onboarding.precourse.albusduke.lotto.domain;
 public record GameResult(int matchingCount, int bonusMatchingCount) {
 
     public boolean matches(int matchingCount, int bonusMatchingCount) {
-        return matchingCount == this.matchingCount && bonusMatchingCount == this.bonusMatchingCount;
+        if (bonusMatchingCount == 0) {
+            return this.matchingCount == matchingCount;
+        }
+        return this.matchingCount == matchingCount && this.bonusMatchingCount == bonusMatchingCount;
     }
 }
