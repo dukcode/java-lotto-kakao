e39cbd9ba3ef582c8f94f0520ae38a6d0221f98e
김덕윤 <duk9741@gmail.com>
2026-02-19 12:02:41 +0900

    refactor: Prize matches 숫자 비교 책임 GameResult로 이동

--- src/main/java/com/kakao/onboarding/precourse/albusduke/lotto/domain/GameResult.java
+++ src/main/java/com/kakao/onboarding/precourse/albusduke/lotto/domain/GameResult.java
@@ -1,4 +1,8 @@
 package com.kakao.onboarding.precourse.albusduke.lotto.domain;
 
 public record GameResult(int matchingCount, int bonusMatchingCount) {
+
+    public boolean matches(int matchingCount, int bonusMatchingCount) {
+        return matchingCount == this.matchingCount && bonusMatchingCount == this.bonusMatchingCount;
+    }
 }
